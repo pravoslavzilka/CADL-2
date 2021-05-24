b7dc44469883204c4c985226cd9e6efd6e9559a2
Pravoslav <pravoslav.zilka@gmail.com>
2021-05-24 23:01:59 +0200

    acess to user data from firabase added

--- lib/pages/AccountPage.dart
+++ lib/pages/AccountPage.dart
@@ -3,13 +3,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 
+final FirebaseAuth auth = FirebaseAuth.instance;
 
 class AccountPage extends StatelessWidget {
 
-  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  final CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('users')
+      .where("uid",isEqualTo: auth.currentUser.uid).snapshots();
+
 
   Future<void> addRecord() {
     CollectionReference records = FirebaseFirestore.instance.collection('records');
+    print(users.where("uid",isEqualTo: auth.currentUser.uid).get());
     return records
         .add({
       'title': "title 1",
@@ -21,23 +28,80 @@ class AccountPage extends StatelessWidget {
 
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children:[
-          Center(
-            child:MaterialButton(
-              child: Text("Sign out ${auth.currentUser.email}"),
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-              },
-            ),
-          ),
-          MaterialButton(
-            child: Text("create a record"),
-            onPressed: addRecord,
+    return StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+        print(snapshot.data.docs[0]["region"]);
+        return Scaffold(
+          body: Column(
+            children: [
+              Center(
+                child: Text(
+                  snapshot.data.docs[0]["full_name"],
+                  style: TextStyle(height:3,fontSize: 55),
+                ),
+              ),
+              cusCon,
+              Container(
+                padding: EdgeInsets.only(top: 65.0),
+                child: emailLabel,
+              )
+            ],
           )
-        ],
-      ),
+        );
+      },
     );
   }
 }
+
+Widget cusCon = Container(
+  margin: EdgeInsets.all(10),
+  padding: EdgeInsets.all(10),
+  decoration: BoxDecoration(
+      color: Colors.teal[100],
+      border: Border.all(
+          color: Colors.teal[100], // Set border color
+          width: 3.0),   // Set border width
+      borderRadius: BorderRadius.all(
+          Radius.circular(10.0)), // Set rounded corner radius
+      boxShadow: [BoxShadow(blurRadius: 10,color: Colors.black,offset: Offset(1,3))] // Make rounded corner of border
+  ),
+  child: Text("My demo styling"),
+);
+
+Widget emailLabel = Container(
+    padding: EdgeInsets.all(10),
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+      color: Colors.teal[100],
+      border: Border.all(
+          width: 3.0,
+        color: Colors.teal[100]
+      ),
+      borderRadius: BorderRadius.all(
+          Radius.circular(25)
+      ),
+      boxShadow: [BoxShadow(blurRadius: 2,color: Colors.black,offset: Offset(1,3))]
+    ),
+    child: Text(
+      auth.currentUser.email,
+      style: TextStyle(
+          fontSize: 16
+      ),
+    )
+);
+
+
+Widget matBut = MaterialButton(
+  child: Text("Sign out"),
+  onPressed: () async {
+    await FirebaseAuth.instance.signOut();
+  },
+);
